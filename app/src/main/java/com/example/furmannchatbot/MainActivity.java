0ade92126fd14fafad5b4c9ca9e2aae5d2802877
Luiz Furmann <furmannluiz@gmail.com>
2024-03-01 15:32:07 -0300

    criado layout do chat

--- app/src/main/java/com/example/furmannchatbot/MainActivity.java
+++ app/src/main/java/com/example/furmannchatbot/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.furmannchatbot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.example.furmannchatbot.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
     }
 }
